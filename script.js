a2251bc7bfc0e441ee7ce005ad03b9c8406bb84f
Anas Bahraoui <80850613+Anas127@users.noreply.github.com>
2023-02-19 21:44:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function func1(){
+    var p = document.getElementById("hidden");
+    p.style.display = "block"; 
+}
+
+function mouseOver(){
+    let elm = document.getElementById("mouse")
+    elm.style.color="red";
+}
+
+function mouseOut(){
+    let elm = document.getElementById("mouse");
+    elm.style.color="#66BFBF";
+}
